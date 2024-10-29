8bf0694b9dad526d0a093558083fa1dccc593561
ネオン様 <122681564+Eineon@users.noreply.github.com>
2024-10-29 21:35:44 +0800

    up

--- docs/source/_lib/lib-script.js
+++ docs/source/_lib/lib-script.js
@@ -23,3 +23,26 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const data = {
+    '#专注': '此效果要求使用者的精神在一定程度上保持集中和自制。',
+    '#操作': '角色必须物理上操作一件物品或比划手势来使用此效果。没有合适肢体的角色无法执行这类动作。',
+    '#攻击': '此效果要求角色发起一次攻击。',
+    '#心灵': '此效果可以干涉目标的心智。这对［无意识］角色无效。',
+    '#绝技': '每一刻最多只能宣言1次［绝技］动作。',
+    '#起手': '只能在尚未宣言过［攻击］或［起手］动作的回合中使用。',
+    '#暗骰': 'GM需要暗中投掷该效果的检定。',
+    '#治愈': '此效果能够修复角色物理上的损伤或精神上的疲劳。',
+    '#附赠': '只要角色满足「前置」条件和等级要求，她就会自动习得此类特技，并且不计入她所能习得的特技总数中。',
+    '#探索': '此效果无法在冲突场景中使用。',
+    '#休整': '执行不具［休整］标签的动作会打断此类动作。',
+  };
+
+  Object.entries(data).forEach(([key, value]) => {
+    document.querySelectorAll(key).forEach(el => {
+      const description = `<div class="mouse-event"><a href="##"><code>${key.slice(1)}</code></a><div class="show">${value}</div></div>`;
+      el.insertAdjacentHTML('afterend', description);
+    });
+  });
+});
